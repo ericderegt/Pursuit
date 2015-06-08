5cbe1187a8e852fee031a0125948298636d351c6
Eric deRegt <ericderegt@gmail.com>
2015-06-08 16:24:49 -0400

    remove comment

--- app/assets/javascripts/components/Course.js.jsx
+++ app/assets/javascripts/components/Course.js.jsx
@@ -19,6 +19,9 @@ var ChapterList = React.createClass({
   render: function() {
     var chapterNodes = this.props.chapters.map(function(chapter, index) {
       return (
+        // `key` is a React-specific concept and is not mandatory for the
+        // purpose of this tutorial. if you're curious, see more here:
+        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
         <Chapter title={chapter.title} content={chapter.content} index={index} link={chapter.link} date={chapter.updated_at} key={index} />
       );
     });
